3756963567ff2bf62db3a15c99ca92679b1258c4
Shahvez <sha685789@gmail.com>
2025-06-03 05:55:08 +0500

    project setup done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 const DBUri = "mongodb+srv://shahvez:Shahvez123@cluster0.w1f3t.mongodb.net";
 
-const express = require("express");
-
-const app = express();
-
-app.get("/", () => {
-  console.log("I am Happy");
-});
-
-app.listen(3000, () => {
-  console.log("server is running");
-});
