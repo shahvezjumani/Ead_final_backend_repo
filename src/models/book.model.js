364bb76a743f8eaf9397b74460b69daa13423166
Shahvez <sha685789@gmail.com>
2025-06-03 11:26:08 +0500

    created book schema using mongoose

---
+++ src/models/book.model.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Book = mongoose.model("Book", bookSchema);
